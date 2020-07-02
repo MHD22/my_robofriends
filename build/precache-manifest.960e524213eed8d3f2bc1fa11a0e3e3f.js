0455c3cf7b31b08dc9047567eb43a18192dec3dc
MHD22 <mhd.sad22@gmail.com>
2020-07-02 10:38:24 +0300

    delete ..

---
+++ build/precache-manifest.960e524213eed8d3f2bc1fa11a0e3e3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2870de7645defb28f1257bc9c3bd10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "1ed02a4ba564bd66beb4",
+    "url": "/static/css/main.63d5ac11.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed02a4ba564bd66beb4",
+    "url": "/static/js/main.ee8694b4.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
